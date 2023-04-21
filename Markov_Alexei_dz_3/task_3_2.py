ebb5201f742009aa78453fa1cd8cc603d5a5896d
Alex Mark <alexvmar@yandex.ru>
2023-04-21 09:37:41 +0300

    change files homework3

--- Markov_Alexei_dz_3/task_3_2.py
+++ Markov_Alexei_dz_3/task_3_2.py
@@ -8,3 +8,22 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
+size_array_n = int(input("Введите размер массива: "))
+search_number_x = int(input("Введите заданное число: "))
+array_lst = list(range(1, size_array_n+1))
+result_number = array_lst[0]
+temp_difference = 0
+temp_difference_min = search_number_x - array_lst[0]
+i = 1
+while i < len(array_lst):
+    temp_difference = search_number_x - array_lst[i]
+    if abs(temp_difference) < abs(temp_difference_min):
+        temp_difference_min = temp_difference
+        result_number = array_lst[i]
+    i += 1
+
+
+print(array_lst)
+print(search_number_x)
+print(f"Самый близкий по величине элемент {result_number}")
