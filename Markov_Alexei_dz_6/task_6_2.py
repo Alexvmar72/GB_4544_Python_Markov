cbd5c6984f401281699be2902c37af3e7c141879
Alex Mark <alexvmar@yandex.ru>
2023-04-27 10:49:31 +0300

    change files homework6

--- Markov_Alexei_dz_6/task_6_2.py
+++ Markov_Alexei_dz_6/task_6_2.py
@@ -1,10 +1,10 @@
 # Задача 32: Определить индексы элементов массива (списка),
 # значения которых принадлежат заданному диапазону
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
-
+import random
 min_value = int(input("Введите минимальное значение диапазона: "))
 max_value = int(input("Введите максимальное значение диапазона: "))
-my_list = [el for el in range(1, 21)]
+my_list = [random.randint(1, 20) for _ in range(20)]
 print(my_list)
 for i in range(len(my_list)):
     if min_value <= my_list[i] <= max_value:
