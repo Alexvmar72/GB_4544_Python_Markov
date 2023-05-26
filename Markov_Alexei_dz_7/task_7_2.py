c4b9ac886416b620b0b34e62fb528e33783ce42e
Alex Mark <alexvmar@yandex.ru>
2023-05-26 09:49:42 +0300

    change files homework7

--- Markov_Alexei_dz_7/task_7_2.py
+++ Markov_Alexei_dz_7/task_7_2.py
@@ -18,8 +18,8 @@
 # 6 12 18 24 30 36
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
-    for i in a:
+    lst_temp = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in lst_temp:
         print(*[f"{x:>3}" for x in i])
 
 
